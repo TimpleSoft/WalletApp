244bf72f334655f775b77d29309825e91fe6d288
Julio Martínez <julio.martinez@timplesoft.es>
2015-06-01 21:19:15 +0100

    initial test case

--- TSOEuroTestClass.m
+++ TSOEuroTestClass.m
@@ -1,19 +1,19 @@
 //
-//  WalletTests.m
-//  WalletTests
+//  TSOEuroTestClass.m
+//  
 //
 //  Created by Timple Soft on 1/6/15.
-//  Copyright (c) 2015 TimpleSoft. All rights reserved.
+//
 //
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
-@interface WalletTests : XCTestCase
+@interface TSOEuroTestClass : XCTestCase
 
 @end
 
-@implementation WalletTests
+@implementation TSOEuroTestClass
 
 - (void)setUp {
     [super setUp];
